5f576994c4c0a198190aaf567851af2f176edd35
892106902@qq.com <892106902@qq.com>
2015-11-27 11:32:03 +0800

    格式化代码

--- src/main/java/cn/bc/test/Main.java
+++ src/main/java/cn/bc/test/Main.java
@@ -1,17 +1,12 @@
 package cn.bc.test;
 
-
-public class Main 
-{
-    public static void main( String[] args )
-    {
+public class Main {
+    public static void main(String[] args) {
         //plus(3,4);
-	System.out.println(plus(3,4));
+        System.out.println(plus(3, 4));
     }
-   
 
-    public static int plus(int a,int b){
-
-	return a+b;	
-   }
+    public static int plus(int a, int b) {
+        return a + b;
+    }
 }
--- src/test/java/cn/bc/test/MainTest.java
+++ src/test/java/cn/bc/test/MainTest.java
@@ -1,18 +1,15 @@
 package cn.bc.test;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-public class MainTest 
-   
-{ 
+
+public class MainTest {
     @Test
-    public void  testPlus(){
-
-      Main a=new Main();
-      int num=a.plus(3,4);
-      assertEquals(7, num);
-	
-
-   }
+    public void testPlus() {
+        Main a = new Main();
+        int num = a.plus(3, 4);
+        assertEquals(7, num);
+    }
 }
